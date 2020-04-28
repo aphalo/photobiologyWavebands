982bb000133c4e8a6aef657697e31af77ef71e43
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2020-04-28 12:24:52 +0300

    Add NDVI() and minor update to docs.

--- R/ndvi.r
+++ R/ndvi.r
@@ -3,7 +3,7 @@
 #' Compute the NDVI from spectral reflectance according to waveband definitions
 #' from standards or corresponding to satellite imagers.
 #'
-#' @param x reflectance_spct or reflectance_mspct object.
+#' @param spct reflectance_spct or reflectance_mspct object.
 #' @param imager character Name of the imager or standard to be used.
 #' @param wb.trim logical Flag telling if wavebands crossing spectral data
 #'   boundaries are trimmed or ignored.
@@ -22,28 +22,31 @@
 #'   \code{\link[photobiology]{reflectance}}.
 #'
 #' @note The value passed as argument to \code{imager} must be a valid argument
-#'   for both \code{\link{Red}} and \code{\link{NIR}}.
+#'   for both \code{\link{Red}} and \code{\link{NIR}}. If the desired return
+#'   value is a data frame, function \code{\link[photobiology]{NDxI}} can be
+#'   used to flexibly compute NDVI and any similar index.
 #'
-#' @return A numeric vector.
+#' @return A numeric vector.  When the wavelength range of \code{spct} does not
+#'   fully overlap with both wavebands \code{NA} is silently returned.
 #'
 #' @export
 #'
-NDVI <- function(x, imager = "LandsatOLI", wb.trim = FALSE) {
-  if (is.generic_mspct(x)) {
-    msaply(x, .fun = NDVI, imager = imager, wb.trim = wb.trim)
-  } else if (is.reflector_spct(x) || is.object_spct(x)) {
+NDVI <- function(spct, imager = "LandsatOLI", wb.trim = FALSE) {
+  if (is.generic_mspct(spct)) {
+    msaply(spct, .fun = NDVI, imager = imager, wb.trim = wb.trim)
+  } else if (is.reflector_spct(spct) || is.object_spct(spct)) {
     # LC8NDVI, LE7NDVI, LT5NDVI, or LT4NDVI
     Red.band <- Red(std = imager)
     NIR.band <- NIR(std = imager)
-    if (wl_min(x) > wl_min(Red.band) || wl_max(x) < wl_max(NIR.band)) {
+    if (wl_min(spct) > wl_min(Red.band) || wl_max(spct) < wl_max(NIR.band)) {
       NA_real_
     } else {
-      Rfr.Red <- as.numeric(reflectance(x, Red.band))
-      Rfr.NIR <- as.numeric(reflectance(x, NIR.band))
+      Rfr.Red <- as.numeric(reflectance(spct, Red.band))
+      Rfr.NIR <- as.numeric(reflectance(spct, NIR.band))
       (Rfr.NIR - Rfr.Red) / (Rfr.NIR + Rfr.Red)
     }
   } else {
-    warning("'NDVI' is not defined for ", class(x))
+    warning("'NDVI' is not defined for ", class(spct))
     NA_real_
   }
 }
